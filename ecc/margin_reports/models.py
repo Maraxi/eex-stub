63fc4d21a696dd997ad462eb91fd46aec35dd737
Maraxi <Maraxi@users.noreply.github.com>
2021-12-13 10:43:25 +0100

    Update models to allow more configuration.
    For each Account margin classes may be specified to be monitored.
    For each Account a list of Email recipients for error emails may be specified.

--- ecc/margin_reports/models.py
+++ ecc/margin_reports/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Account(models.Model):
+    Name = models.CharField(max_length=100)
 
 
 class Intraday(models.Model):
     Clearing_Member = models.CharField(max_length=100)
-    Account = models.CharField(max_length=40)
+    Account = models.ForeignKey(Account, on_delete=models.CASCADE)
     Margin_Class = models.CharField(max_length=20)
     Margin = models.DecimalField(decimal_places=3, max_digits=13)
     Report_Date = models.DateField()
@@ -13,8 +15,26 @@ class Intraday(models.Model):
 
 class EndofDay(models.Model):
     Clearing_Member = models.CharField(max_length=100)
-    Account = models.CharField(max_length=40)
+    Account = models.ForeignKey(Account, on_delete=models.CASCADE)
     Margin_Class = models.CharField(max_length=20)
     Margin = models.DecimalField(decimal_places=3, max_digits=13)
     Report_Date = models.DateField()
     Report_Time = models.TimeField()
+
+
+class MarginClassSurveilance(models.Model):
+    Account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    SPAN = models.BooleanField()
+    IMSM = models.BooleanField()
+    CESM = models.BooleanField()
+    AMPO = models.BooleanField()
+    AMEM = models.BooleanField()
+    AMCO = models.BooleanField()
+    AMCU = models.BooleanField()
+    AMWI = models.BooleanField()
+    DMEM = models.BooleanField()
+
+
+class ErrorEmailRecipients(models.Model):
+    Account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    Recipient = models.CharField(max_length=100)
