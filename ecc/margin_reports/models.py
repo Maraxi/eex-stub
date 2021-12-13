daece0e93da5d25d59551137b1741207d682a179
Maraxi <Maraxi@users.noreply.github.com>
2021-12-13 12:08:18 +0100

    Fix models.

--- ecc/margin_reports/models.py
+++ ecc/margin_reports/models.py
@@ -11,6 +11,7 @@ class Intraday(models.Model):
     Margin_Class = models.CharField(max_length=20)
     Margin = models.DecimalField(decimal_places=3, max_digits=13)
     Report_Date = models.DateField()
+    Report_Time = models.TimeField()
 
 
 class EndofDay(models.Model):
@@ -19,20 +20,19 @@ class EndofDay(models.Model):
     Margin_Class = models.CharField(max_length=20)
     Margin = models.DecimalField(decimal_places=3, max_digits=13)
     Report_Date = models.DateField()
-    Report_Time = models.TimeField()
 
 
 class MarginClassSurveilance(models.Model):
-    Account = models.ForeignKey(Account, on_delete=models.CASCADE)
-    SPAN = models.BooleanField()
-    IMSM = models.BooleanField()
-    CESM = models.BooleanField()
-    AMPO = models.BooleanField()
-    AMEM = models.BooleanField()
-    AMCO = models.BooleanField()
-    AMCU = models.BooleanField()
-    AMWI = models.BooleanField()
-    DMEM = models.BooleanField()
+    Account = models.OneToOneField(Account, on_delete=models.CASCADE)
+    SPAN = models.BooleanField(default=True)
+    IMSM = models.BooleanField(default=True)
+    CESM = models.BooleanField(default=True)
+    AMPO = models.BooleanField(default=True)
+    AMEM = models.BooleanField(default=True)
+    AMCO = models.BooleanField(default=True)
+    AMCU = models.BooleanField(default=True)
+    AMWI = models.BooleanField(default=True)
+    DMEM = models.BooleanField(default=True)
 
 
 class ErrorEmailRecipients(models.Model):
